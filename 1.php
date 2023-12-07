c248fb82168bf639325a04790353fee5879ce8a2
lourdesgui <lourdes.guiretti@hotmail.com>
2023-12-07 18:52:21 -0300

    form

--- 1.php
+++ 1.php
@@ -20,6 +20,6 @@ $subject = "Consultas web de $nombre";
 $header = "From: $correo \r\n";
 $header .= "Content-Type: text/plain; charset=UTF-8";
 mail($recipient, $subject, $formcontent, $header) or die("Error!");
-header("Location: gracias.html");
+header("Location: index.html");
 
 ?>
