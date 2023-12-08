353b9b1b8ed7f12eeffb4bfa4e3dd139bac93be1
lourdesgui <lourdes.guiretti@hotmail.com>
2023-12-08 11:03:11 -0300

    form

---
+++ CSS/api/1.php
@@ -0,0 +1,25 @@
+<?php
+
+$name = $_POST['fullname'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+
+$formcontent="
+    fullname: $name \n
+    email: $email \n
+    phone: $phone \n
+    message: $message 
+
+";
+
+$recipient = "lourdes.guiretti@hotmail.com", $email;
+
+$subject = "Consultas web de $name";
+
+$header = "From: $email \r\n";
+$header .= "Content-Type: text/plain; charset=UTF-8";
+mail($recipient, $subject, $formcontent, $header) or die("Error!");
+header("Location: index.html");
+
+?>
--- README.md
+++
@@ -1,2 +0,0 @@
-# englisheverywhere
-# englisheverywhere
